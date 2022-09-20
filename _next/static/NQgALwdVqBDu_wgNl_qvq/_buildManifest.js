726595d7718a6dafb508a600770d8a6671706ec6
ruchI9897 <59597701+ruchI9897@users.noreply.github.com>
2022-09-20 15:16:58 +0000

    Deploying to gh-pages from @ ruchI9897/IraSoft@00a1481c2157c0412bf4a8efb5ea0c1e0fe05ceb 🚀

---
+++ _next/static/NQgALwdVqBDu_wgNl_qvq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/96f32245a637a911.css","static/chunks/pages/index-026816d5f9b17914.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
