c36885942000590935a1ae8aa07a516baf85cf59
ruchI9897 <ruchi.anand9897@gmail.com>
2022-09-20 20:57:38 +0530

    changed style

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src='underConst.jpg' width={'500px'}/>
-      <h1 style={{color:'#E63946', fontFamily:'fangsong'}}>Thanks for visiting, IraSoft will be live soon...</h1>
-      <h2 style={{fontFamily:'fangsong'}}>Stay tuned!!</h2>
+      <div style={{display:'flex','justify-content': 'center','align-items': 'center','flex-direction': 'column'}}>
+        <img src='underConst.jpg' width={'500px'}/>
+        <h1 style={{color:'#E63946', fontFamily:'fangsong'}}>Thanks for visiting, IraSoft will be live soon...</h1>
+        <h2 style={{fontFamily:'fangsong'}}>Stay tuned!!</h2>
+      </div>
     </div>
   )
 }
