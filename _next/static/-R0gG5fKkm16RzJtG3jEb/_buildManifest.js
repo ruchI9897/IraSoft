a8c18086f0d264501d77fdcb66c7cde8f43c7053
ruchI9897 <59597701+ruchI9897@users.noreply.github.com>
2022-09-20 15:28:36 +0000

    Deploying to gh-pages from @ ruchI9897/IraSoft@c36885942000590935a1ae8aa07a516baf85cf59 🚀

--- _next/static/-R0gG5fKkm16RzJtG3jEb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a4ad007a7fe9b37b.css","static/chunks/pages/index-026816d5f9b17914.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/0WcqnmJ0szPtSE5BwXDwE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a4ad007a7fe9b37b.css","static/chunks/pages/index-b245932dbfb0f10c.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
