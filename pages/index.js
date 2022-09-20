f1e8718bfa9907c1320e0c5f15cb7011b453b7b9
ruchI9897 <ruchi.anand9897@gmail.com>
2022-09-20 13:31:22 +0530

    Added Site under construction place holder

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src='undercons.jpg' width={'500px'}/>
+      <img src='underConst.jpg' width={'500px'}/>
+      <h1 style={{color:'#E63946', fontFamily:'fangsong'}}>Thanks for visiting, IraSoft will be live soon...</h1>
+      <h2 style={{fontFamily:'fangsong'}}>Stay tuned!!</h2>
     </div>
   )
 }
