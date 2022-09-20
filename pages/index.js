dedf10bb8fe0c05ca46aa8c71139dcb4fa2cb370
ruchI9897 <ruchi.anand9897@gmail.com>
2022-09-20 21:26:35 +0530

    Changed contact details

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div style={{display:'flex','justify-content': 'center','align-items': 'center','flex-direction': 'column'}}>
-        <img src='undercons.jpg' width={'500px'}/>
+        <img src='undercons.jpg' width={'400px'}/>
         <h1 style={{color:'#E63946', fontFamily:'fangsong'}}>Thanks for visiting, IraSoft will be live soon...</h1>
         <h2 style={{fontFamily:'fangsong'}}>Stay tuned!!</h2>
-        <h3>Contact: jitendra@IraSoft.in/ruchi@IraSoft.in</h3>
+        <h3>Contact: jitendra@irasoft.in</h3>
       </div>
     </div>
   )
